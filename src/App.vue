6903fbd4c7a66aa0ad42827123c8a4b2b6241f55
callister24 <chandracallister@gmail.com>
2025-01-13 05:06:41 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ onMounted(() => {
 </script>
 
 <template>
-	<div class="text-container">Airdrop ASC</div>
+	<div class="text-container">Airdrop</div>
 	<div class="rain" ref="rainContainer"></div>
 </template>
 
